5d5f67d168b0405f8cc76d8f015b55be7d091855
Tarun Mohandas <tarun.mohandas@gmail.com>
2019-11-13 02:41:09 +0530

    players can make move now through api

--- routes/api/player.js
+++ routes/api/player.js
@@ -32,4 +32,58 @@ router.post('/',(req,res) => {
     });
 });
 
+router.post('/:playerId/join/:gameId',(req,res) => {
+    var playerId = ObjectId(req.params.playerId);
+    var gameId = ObjectId(req.params.gameId);
+    var playerQuery = {};
+    playerQuery._id = playerId;
+    var gameQuery = {};
+    gameQuery._id = gameId;
+    Player.find(playerQuery).then((players)=> {
+        if(players.length === 0)
+        {
+            return res.status(400).json({msg:"Invalid player ID", input:req.params.playerId});
+        }
+        Game.find(gameQuery).then((games) => {
+            if(games.length === 0)
+            {
+                return res.status(400).json({msg:"Invalid game ID", input:req.params.gameId});
+            }
+            var game = games[0];
+            var pj = game.Players_Joined;
+            var gameUpdateObj = {};
+            if(pj >= 2)
+            {
+                return res.status(400).json({msg:"All players joined in the game."});
+            }
+            if(pj === 0)
+            {
+                gameUpdateObj.Player_One = playerId;
+            }
+            else if(pj === 1)
+            {
+                gameUpdateObj.Player_Two = playerId;
+            }
+            gameUpdateObj.Players_Joined = (pj+1);
+            updateQueryObj = {};
+            updateQueryObj._id = gameId;
+            Game.updateOne(updateQueryObj,gameUpdateObj, (err) => {
+                if(err)
+                {
+                    return res.status(500).json({msg:"Some problem occurred while updating game"});
+                }
+                return res.status(200).json({msg:"Player joined successfully"});
+            });
+        }).catch((err) => {
+            console.log(err);
+            return res.status(500).json({msg:"Problem with fetching games from the database"});
+        });
+
+    }).catch((err) => {
+        console.log(err);
+        return res.status(500).json({msg:"Problem with fetching games from the database"});
+    });
+    
+});
+
 module.exports = router;
